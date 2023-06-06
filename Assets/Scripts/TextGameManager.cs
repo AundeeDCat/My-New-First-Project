3908ddc46c7e1f734c84f4cfcfec8c28b02e52df
AundeeDCat <119554562+AundeeDCat@users.noreply.github.com>
2023-06-06 14:34:04 +0800

    Main Menu
    Added a Main Menu

--- Assets/Scripts/TextGameManager.cs
+++ Assets/Scripts/TextGameManager.cs
@@ -10,7 +10,7 @@ public class TextGameManager : MonoBehaviour
     public string storyText = "Erwin, you, have awoken on a strange land. You're recollection of how you ended up here in this situation is fuzzy. You seem to be on the shore of a beach.  There is debris everywhere in the sand. As you observe your surroundigs, a broken sail reminds you of someone. Your partner Eru... Where is he? You notice there are a few places to look, some rock formations, somwhere in the tree line or perhaps floating somewhere in the water... Where do you go?";
     public int health_value = 10;
     public int stamina_value = 10;
-    public GameObject Level_1_Choices, Level_2_Choices;
+    public GameObject Main_Menu, Levels, Level_1_Choices, Level_2_Choices;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +18,9 @@ public class TextGameManager : MonoBehaviour
         storyTextMeshPro.text = storyText;
         healthTextMeshPro.text = health_value.ToString();
         staminaTextMeshPro.text = stamina_value.ToString();
+
+        Main_Menu.SetActive(true);
+        Levels.SetActive(false);
     }
 
     // Update is called once per frame
@@ -28,6 +31,20 @@ public class TextGameManager : MonoBehaviour
         staminaTextMeshPro.text = stamina_value.ToString();
     }
 
+    // Main Menu Commands
+    public void Begin()
+    {
+        Main_Menu.SetActive(false);
+        Levels.SetActive(true);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+
+    // Level 1 Commands
     public void Rocks()
     {
         storyText = "You check the rocks. Luckily, there is no sign of him there. The rocks and waves would've turn him to shreds. You cut your hand on a rock too. You got a little tired from searching.";
